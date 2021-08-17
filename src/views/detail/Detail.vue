2707476ca5dd0cf45837e90bedaf2d0d2da5be2b
ITlaoY <shashoulaoy@qq.com>
2021-08-17 15:20:01 +0800

    第八次初始化项目

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="detail">
-    <detail-nav-bar />
-    <detail-swiper :top-images="topImages" />
-    <detail-base-info :goods="goods"></detail-base-info>
+    <detail-nav-bar class="detail-nav" />
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages" />
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop" />
+      <detail-comment-info :detail-info="detailInfo" @imageLoad="imageLoad" />
+      <detail-param-info :param-info="paramInfo" />
+    </scroll>
   </div>
 </template>
 
@@ -10,8 +15,13 @@
 import DetailNavBar from "./childComps/DetailNavBar.vue";
 import DetailSwiper from "./childComps/DetailSwiper.vue";
 import DetailBaseInfo from "./childComps/DetailBaseInfo.vue";
+import DetailShopInfo from "./childComps/DetailShopInfo.vue";
+import DetailCommentInfo from "./childComps/DetailCommentInfo.vue";
+import DetailParamInfo from "./childComps/DetailParamInfo.vue";
 
-import { getDetail, Goods } from "../../network/detail";
+import Scroll from "../../components/common/scroll/Scroll.vue";
+
+import { getDetail, Goods, Shop, GoodsParam } from "../../network/detail";
 
 export default {
   name: "Detail",
@@ -20,6 +30,9 @@ export default {
       iid: null,
       topImages: [],
       goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
     };
   },
   created() {
@@ -37,15 +50,49 @@ export default {
         data.columns,
         data.shopInfo.services
       );
+      //获取店铺信息
+      this.shop = new Shop(data.shopInfo);
+
+      //保存商品详情数据
+      this.detailInfo = data.detailInfo;
+
+      //获取参数信息
+      this.paramInfo = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
     });
+  },
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh();
+    },
   },
   components: {
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
+    DetailShopInfo,
+    Scroll,
+    DetailCommentInfo,
+    DetailParamInfo,
   },
 };
 </script>
 
-<style>
+<style scoped>
+#detail {
+  position: relative;
+  z-index: 20;
+  background-color: #fff;
+  height: 100vh;
+}
+.content {
+  height: calc(100% - 44px);
+}
+.detail-nav {
+  position: relative;
+  z-index: 101;
+  background-color: #fff;
+}
 </style>
